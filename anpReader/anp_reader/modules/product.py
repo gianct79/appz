93021cb48f2943fa262f6de55a8ef02d472d6414
Giancarlo Tomazelli <gianct79@gmail.com>
2015-05-11 19:58:19 -0300

    added prices from federation units

--- anpReader/anp_reader/modules/product.py
+++ anpReader/anp_reader/modules/product.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 PRODUCT_TYPES = {u'ETANOL HIDRATADO ADITIVADO': (810101002, None),
-                 u'ETANOL HIDRATADO COMUM': (810101001, 643),
+                 u'ETANOL HIDRATADO COMUM': (810101001, '643'),
                  u'GASOLINA C ADITIVADA': (320102002, None),
-                 u'GASOLINA C COMUM': (320102001, 487),
+                 u'GASOLINA C COMUM': (320102001, '487'),
                  u'GASOLINA C PREMIUM': (320102003, None),
-                 u'GAS NATURAL VEICULAR': (220101005, 476),
+                 u'GAS NATURAL VEICULAR': (220101005, '476'),
                  u'OLEO DIESEL B S10 - ADITIVADO': (820101033, None),
-                 u'OLEO DIESEL B S10 - COMUM': (820101034, 812),
+                 u'OLEO DIESEL B S10 - COMUM': (820101034, '812'),
                  u'OLEO DIESEL B S500 - ADITIVADO': (820101013, None),
-                 u'OLEO DIESEL B S500 - COMUM': (820101012, 532)}
+                 u'OLEO DIESEL B S500 - COMUM': (820101012, '532')}
 
-ANP_CODES = {643: 810101001,
-             487: 320102001,
-             476: 220101005,
-             812: 820101034,
-             532: 820101012}
+ANP_CODES = {'643': 810101001,
+             '487': 320102001,
+             '476': 220101005,
+             '812': 820101034,
+             '532': 820101012}
--- anpReader/anp_reader/modules/util.py
+++ anpReader/anp_reader/modules/util.py
@@ -20,3 +20,34 @@ def count_weeks(from_date=date.today()):
     d0 = date(1999, 7, 1)
     delta = from_date - d0
     return (delta.days / 7) + 1
+
+
+state_map = {
+    u'acre': 'AC',
+    u'alagoas': 'AL',
+    u'amapa': 'AP',
+    u'amazonas': 'AM',
+    u'bahia': 'BA',
+    u'ceara': 'CE',
+    u'distrito federal': 'DF',
+    u'espirito santo': 'ES',
+    u'goias': 'GO',
+    u'maranhao': 'MA',
+    u'mato grosso': 'MT',
+    u'mato grosso do sul': 'MS',
+    u'minas gerais': 'MG',
+    u'para': 'PA',
+    u'paraiba': 'PB',
+    u'parana': 'PR',
+    u'pernambuco': 'PE',
+    u'piaui': 'PI',
+    u'rio de janeiro': 'RJ',
+    u'rio grande do norte': 'RN',
+    u'rio grande do sul': 'RS',
+    u'rondonia': 'RO',
+    u'roraima': 'RR',
+    u'santa catarina': 'SC',
+    u'sao paulo': 'SP',
+    u'sergipe': 'SE',
+    u'tocantins': 'TO'
+}
