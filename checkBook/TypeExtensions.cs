db34cfeff673df067273839f38c524c8601a3360
Giancarlo Tomazelli <tomazell@hp.com>
2015-06-23 08:55:21 -0300

    change dicts to vectors; small fixes;

--- checkBook/TypeExtensions.cs
+++ checkBook/TypeExtensions.cs
@@ -11,56 +11,53 @@ namespace checkBook
 {
     public class Extension
     {
-        static Dictionary<int, string> units = new Dictionary<int, string>();
-        static Dictionary<int, string> d_units = new Dictionary<int, string>();
-        static Dictionary<int, string> c_units = new Dictionary<int, string>();
+        static string[] units;
+        static string[] d_units;
+        static string[] c_units;
 
         static Extension()
         {
-            units[1] = "um"; units[2] = "dois"; units[3] = "três";
-            units[4] = "quatro"; units[5] = "cinco"; units[6] = "seis";
-            units[7] = "sete"; units[8] = "oito"; units[9] = "nove";
-
-            d_units[10] = "dez"; d_units[11] = "onze"; d_units[12] = "doze";
-            d_units[13] = "treze"; d_units[14] = "quatorze"; d_units[15] = "quinze";
-            d_units[16] = "dezesseis"; d_units[17] = "dezessete"; d_units[18] = "dezoito";
-            d_units[19] = "dezenove"; d_units[20] = "vinte"; d_units[30] = "trinta";
-            d_units[40] = "quarenta"; d_units[50] = "cinquenta"; d_units[60] = "sessenta";
-            d_units[70] = "setenta"; d_units[80] = "oitenta"; d_units[90] = "noventa";
-
-            c_units[1] = "cento"; c_units[2] = "duzentos"; c_units[3] = "trezentos";
-            c_units[4] = "quatrocentos"; c_units[5] = "quinhentos"; c_units[6] = "seiscentos";
-            c_units[7] = "setecentos"; c_units[8] = "oitocentos"; c_units[9] = "novecentos";
+            units = new string[] { string.Empty, "um", "dois", "três", "quatro", "cinco",
+                "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze",
+                "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
+            d_units = new string[] { string.Empty, string.Empty, "vinte", "trinta", "quarenta",
+                "cinquenta", "sessenta", "setenta", "oitenta", "noventa" };
+            c_units = new string[] { string.Empty, "cento", "duzentos", "trezentos", "quatrocentos",
+                "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
         }
 
         public static string ToLongString(int value)
         {
+            if (value == 100)
+                return "cem";
+
             StringBuilder sb = new StringBuilder();
-            int c = value / 100;
-            if (c_units.ContainsKey(c))
+
+            int d;
+            int c = Math.DivRem(value, 100, out d);
+            if (c > 0)
             {
                 sb.Append(c_units[c]);
-                sb.Append(" e ");
+                if (d > 0)
+                    sb.Append(" e ");
             }
 
-            int d = value % 100;
-            if (d_units.ContainsKey(d))
+            if (d < 20)
             {
-                sb.Append(d_units[d]);
+                sb.Append(units[d]);
             }
             else
             {
                 int u;
-                d = Math.DivRem(d, 10, out u) * 10;
-                if (d_units.ContainsKey(d))
+                d = Math.DivRem(d, 10, out u);
+                if (d > 0)
                 {
                     sb.Append(d_units[d]);
-                    sb.Append(" e ");
+                    if (u > 0)
+                        sb.Append(" e ");
                 }
-                if (units.ContainsKey(u))
-                {
+                if (u > 0)
                     sb.Append(units[u]);
-                }
             }
 
             return sb.ToString();
@@ -78,8 +75,6 @@ namespace checkBook
 
             if (v == 1)
                 return "um real";
-            if (v == 100)
-                return "cem reais";
 
             StringBuilder sb = new StringBuilder();
 
