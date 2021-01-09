7cf06b3a2955817140c9b01621b4c557b2d43c9e
Giancarlo Tomazelli <giancarlo.tomazelli@eprintit.com>
2021-01-09 16:07:46 -0500

    DOXBox ppp

---
+++ dosbox-ppp/README.md
@@ -0,0 +1,50 @@
+# This is PPP on DOSBox
+
+## Requirements
+
+* Linux Box (WSL2 can work as well)
+* DOSBox
+* [Windows 3.11|https://winworldpc.com/product/windows-3/311]
+* [Trumpet Winsock|https://winworldpc.com/product/trumpet-winsock/3x]
+* [Netscape Navigator|https://winworldpc.com/product/netscape-navigator/2x]
+
+* On Linux box:
+  * Install pppd.
+  * Install socat.
+
+* On DOSBox:  
+  * Change serial configuration:
+
+```
+[serial]
+serial1=modem listenport:2323
+serial2=dummy
+serial3=disabled
+serial4=disabled
+```
+
+## Running
+
+* Run `ppp.sh` as root (original is [here|https://gist.github.com/mainframed/2300903d9cc259a2a2ab431ca152dffc]):
+
+  * Change line 65 from "10.10.0.200:10.10.0.201" to two IP addresses that are routable on home network.
+  * The second address (10.10.0.201) will be the Windows 3.11 IP address.
+
+* Setup Trumpet Winsock:
+
+  * IP address: 0.0.0.0.
+  * DNS server: 8.8.8.8 (or 8.8.4.4).
+
+* Connect with Trumpet Winsock:
+  * Dialler > Manual login.
+  * At the prompt type:
+
+```
+AT # (if error, type AT again)
+
+AT
+
+ATDT <LINUX_IP_ADDRESS>
+```
+
+  * Hit escape and PPP is connected (notice the assigned IP address).
---
+++ dosbox-ppp/ppp.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Heavily Modified from: https://www.dosbox.com/wiki/PPP_configuration_on_linux_host
+#
+# Usage:
+# sudo ./ppp.sh
+#
+# This script makes it so you can browse the net with DOSBox and Trumpet Winsock in
+# Windows 3.11
+#
+# LINUX:
+#   To use this script simply change the IP addresses below to two unused IP addresses on your network
+#   then run with root (needed for port 23/proxyarp)
+#
+# WINDOWS 3.11:
+#   Install Trumpet Winsock
+#   Click on 'Dialer->Manual Login'
+#   Type: AT (if you see 'ERROR' type AT again)
+#   ATDT <LINUX IP ADDRESS>
+#   e.g. ATDT 10.10.10.10
+#   You should see 'CONNECT'
+#   Hit the Escape button and your good to go!
+#
+# DOSBox Config:
+#   Add this to the bottom of your config
+#   [serial]
+#   serial1=modem listenport:2323
+#   serial2=dummy
+#   serial3=disabled
+#   serial4=disabled
+#
+
+grep -q 1 /proc/sys/net/ipv4/ip_forward || \
+    ( echo 1 1>/proc/sys/net/ipv4/ip_forward )
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "** Trapped CTRL-C"
+    echo "** Shutting down ISP"
+    pkill -9 pppd
+    [ -e "/tmp/trumpet" ] && rm "/tmp/trumpet"
+    exit 0
+}
+
+echo "** Creating fake ISP"
+echo "** Using Serial /tmp/trumpet"
+
+while true
+do
+    if sleep 0.1 && pgrep socat > /dev/null 2>&1
+    then
+        sleep 0.1
+    else
+        echo "** Starting socat listener on port 23"
+        socat TCP4-LISTEN:23 PTY,link="/tmp/trumpet" &
+    fi
+    sleep 0.5
+    if pgrep pppd > /dev/null 2>&1
+    then
+        sleep 1
+    else
+        echo "** Starting pppd"
+        pppd "/tmp/trumpet" defaultroute mtu 576 192.168.2.52:192.168.2.53 login proxyarp > /dev/null 2>&1
+    fi
+done
